fe209fef38070cc6d8602daff4120a62576b84e4
Ch0sen1 <45779379+Ch0sen1@users.noreply.github.com>
2020-03-07 06:26:54 -0500

    Add files via upload

---
+++ IBM CAPSTONE.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # This is for IBM DS CAPSTONE
+
+# #### looking for intern, if you have some suggestion/referral pm me: resax39@gmail.com TAT
+
+# In[1]:
+
+
+import pandas as pd
+import numpy as np
+
+
+# In[2]:
+
+
+print ("Hello Capstone Project Course!")
+
+
+# In[ ]:
+
+
+
+
